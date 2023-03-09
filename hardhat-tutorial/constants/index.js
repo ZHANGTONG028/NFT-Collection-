37fe3f16c284bd4fc28478f31fda67857d09fa41
Goodman028 <86427856+ZHANGTONG028@users.noreply.github.com>
2023-03-09 16:32:32 +0800

    ceshi

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = '0x35E2d994b317c92f1D9B2DAeB8d1BbA65a5814c5';
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
